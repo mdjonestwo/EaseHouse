0b8d35ff39585db1313e23f2003803f6875a25b9
mdjonestwo <mdjones2@protonmail.com>
2020-11-06 12:57:33 -0500

    addPalletModal added

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ $(document).ready(function(){
 
     $("td").click(function(){
       var redbground = $(this).addClass("red")
-      
-      
-      
+      var addPalletModal = $("#addPalletModal").toggleClass("is-active")
+      /*
       if(redbground && confirm("Do you want to add a pallet here?") ){
         var pNameQuant = prompt("Enter product name and quantity.")
         console.log(pNameQuant)
@@ -14,16 +13,22 @@ $(document).ready(function(){
         alert("You have no products to add.")
         $(this).addClass("beige")
       }
-
       }); 
   
       $("#dryCooler").on("click", function () {
         $("#dryCoolerLayout").toggleClass("is-hidden");
         $("#dryCooler").toggleClass("has-text-grey");
       });
-      
+  
       $("#trailer").on("click", function () {
         $("#trailerLayout").toggleClass("is-hidden");
         $("#trailer").toggleClass("has-text-grey");
       });
-  }); 
+      */
+     return redbground, addPalletModal
+  })
+
+  $("#modalClose").click(function(){
+    $("#addPalletModal").removeClass("is-active")
+  })
+}); 
