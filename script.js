dbfa7b04581f820d515fbef5a9f20b261c221eed
mdjonestwo <mdjones2@protonmail.com>
2020-11-06 00:37:21 -0500

    dry cooler toggle on

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
     var td = $("td")
 
+
     $("td").click(function(){
       var redbground = $(this).toggleClass("red")
       return redbground
       }); 
   
+      $("#dryCooler").on("click", function () {
+        $("#dryCoolerLayout").toggleClass("is-hidden");
+        $("#dryCooler").toggleClass("has-text-grey");
+      });
+      
   }); 
