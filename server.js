3a35fbc2ecef59aea9b5be45b6519ec2a6b9fa60
mdjonestwo <mdjones2@protonmail.com>
2021-07-27 23:14:53 -0400

    updated routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
@@ -22,6 +23,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/happydirtdb", {
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
 
 app.listen(PORT, () => {
   console.log(`App is listening on port: ${PORT}`);
