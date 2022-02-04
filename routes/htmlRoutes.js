de9e64cf9936457f8623532b11065466d22ad129
mdjonestwo <mdjones2@protonmail.com>
2022-02-04 00:12:07 -0500

    update changed html coolers route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,18 +2,12 @@ const path = require("path");
 
 module.exports = (app) => {
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/../public/index.html"));
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
 
   app.get("/coolers", (req, res) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
     res.sendFile(path.join(__dirname + "/../public/app.html"));
-=======
-    res.sendFile(path.join(__dirname + "/../public/cooler.html"));
->>>>>>> 60ba99b12b728f355dba2daebfb5899a16e79c47
-=======
-    res.sendFile(path.join(__dirname + "/../public/cooler.html"));
->>>>>>> 60ba99b12b728f355dba2daebfb5899a16e79c47
+
+    // res.sendFile(path.join(__dirname + "/../public/cooler.html"));
   });
 };
