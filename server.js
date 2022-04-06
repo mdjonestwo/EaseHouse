040c1a5bf4d9b594aa71ccf8b2a7a11d94a9e19a
mdjonestwo <mdjones2@protonmail.com>
2022-04-06 12:12:48 -0400

    deploying test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
+const {MongoClient} = require('mongodb')
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,9 +15,30 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// async function main(){
+//   const uri = "mongodb+srv://MrJones:7vr4HKqZjazXXRTR@cluster0.iqlmk.mongodb.net/happydirtdb?retryWrites=true&w=majority"
+
+//   const client = new MongoClient(uri)
+
+//   try {
+//     await client.connect();
+
+//     await listDatabases(client);
+
+//   } catch (e) {
+//     console.error(e);
+//   } finally {
+//     await client.close;
+//   }
+
+// }
+
+// main().catch(console.error);
 
 
-mongoose.connect(proccess.env.MONGODB_URI || 'mongodb://127.0.0.1/happydirtdb', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/happydirtdb', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log("Connected to Database");
 }).catch((err) => {
     console.log("Not Connected to Database ERROR! ", err);
